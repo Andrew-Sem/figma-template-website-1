32ab00594b4876c3eea640ad404e8840e207c43c
Andrew-Sem <avvyayvayva@gmail.com>
2023-02-04 23:51:26 +0300

    add fonts

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import {FC} from 'react';
+
+export const Header: FC = () => {
+    return (
+        <div className={"flex font-title text-2xl"}>
+            <div>Global Talk</div>
+        </div>
+    );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily:{
+      title: ['"Dela Gothic One"', 'sans-serif'],
+      base: ['Montserrat', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
